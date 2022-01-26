c79a8f28277dfc9558d2b4ff674be40554474e72
emilio-lobato <49325994+emilio-lobato@users.noreply.github.com>
2022-01-26 08:53:31 -0800

    Update 02_EFA.R

--- scripts/02_EFA.R
+++ scripts/02_EFA.R
@@ -5,26 +5,20 @@ library(pander)
 library(gvlma)
 library(tables)
 library(here)
-
 library(gghighlight)
+library(lavaan)
 
 data_dir = here("data")
 
 source(here('R', 'vis_labels.R'))
 source(here('R', 'loading_table.R'))
 
-panderOptions("table.split.table",Inf)
-panderOptions("round",2)
-panderOptions("keep.trailing.zeros",TRUE)
-panderOptions("table.emphasize.rownames", FALSE)
-panderOptions("table.alignment.rownames", "left")
-panderOptions("missing","")
-
 ## Load data ----
 d_clean = read_rds(here(data_dir, 'data.Rds'))
 
 #EFA for the ViS items, checking factor structure; need to check if N is large enough for a split to do CFA on
 d_vis <- subset(d_clean, select = c(ViS01:ViS36)) |> 
+    na.omit() |> 
     mutate(across(everything(), as.numeric)) |> 
     set_names(vis_labels$tag)
 
@@ -95,28 +89,44 @@ d_vis_efa |>
     coord_flip() +
     theme_minimal()
 
-
+## checking EFA assumptions ----
+cor.matrix <- cor(d_vis_efa)
+bartlett.test(d_vis_efa) #testing whether correlation matrix is significantly different from identity matrix
+KMO(d_vis_efa) #checking adequacy of sample size
+det(cor.matrix) #checking for possible multicollinearity
 
 ## EFA ----
 vis_fa <- fa.parallel(d_vis_efa, fm = "minres", fa = "fa")
 
-#if N is not big enough to reasonably split for EFA and CFA, then just EFA will suffice
-# vis_fa <- fa.parallel(d_vis, fm = "minres", fa = "fa")
-
 #check item loadings for N factor solutions (look for substantial cross-loadings or no substantial loadings)
-#do for as many factor solution options as seems prudent; what is recommended by fa.parallel, visual inspection of scree, or eigenvectors
+#do for as many factor solution options as seems prudent; what is recommended by fa.parallel, visual inspection of scree, or eigenvalues
 three_factor <- fa(d_vis_efa, nfactors = '3', rotate ="varimax")
 six_factor <- fa(d_vis_efa, nfactors = '6', rotate ="varimax")
 #writing factor loadings to a csv for easier inspection
 loading_table(three_factor, path = here(data_dir, "three_factor_loadings.csv"))
 loading_table(six_factor,   path = here(data_dir, "six_factor_loadings.csv"))
 
-
-#CFA if sample is big enough to do EFA on first half and CFA on second half
+## CFA ----
 #lavaan package for CFA
 #specify the items in each latent variable/factor
-model <- ' factor_1 =~ x1 + x2 + x3
-           factor_2 =~ x4 + x5 + x6
-           etc. '
-fit <- cfa(model, data = d_vis_cfa)
-summary(fit, fit.measures = TRUE)
+
+#six factor model recommended by EFA parallel analysis and inflexion point on scree plot
+six_factor_model <- ' factor_1 =~ item1 + item2
+                    factor_2 =~ item3 + item4
+                    factor_3 =~ item5 + item6
+                    factor_4 =~ item7 + item8
+                    factor_5 =~ item9 + item10
+                    factor_6 =~ item11 + item12
+                    '
+fit6 <- cfa(six_factor_model, data = d_vis_cfa)
+summary(fit6, fit.measures = TRUE)
+fitmeasures(fit6, c('chisq','cfi','rmsea','rmsea.ci.upper','srmr','agfi'))
+
+#three factor model based on eigenvalues > 1
+three_factor_model <- ' factor_1 =~ item1 + item2
+                        factor_2 =~ item3 + item4
+                        factor_3 =~ item 5 + item6
+                        '
+fit3 <- cfa(three_factor_model, data = d_vis_cfa)
+summary(fit3, fit.measures = TRUE)
+fitmeasures(fit3, c('chisq','cfi','rmsea','rmsea.ci.upper','srmr','agfi'))
